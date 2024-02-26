1eeddf2e8c2aa9c29bca52f9123239722fa9d3e5
sajin amatya <sajinamatya88@gmail.com>
2024-02-26 21:56:03 +0545

    second

--- Web scraping/main.py
+++ Web scraping/main.py
@@ -1,24 +1,30 @@
-import pandas as pd
-import numpy as np
+# import pandas as pd
+# import numpy as np
 import requests
-from requests import get
+
 from bs4 import BeautifulSoup
-from time import sleep
-from random import randint
+# from time import sleep
+# from random import randint
+
 headerss = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36', 'Accept-langauge': 'en-US, en;q=0.5'}
-page = requests.get('https://www.daraz.com.np/laptops/?page=1&sort=order&spm=a2a0e.searchlistcategory.cate_5.4.655112fcVrPayV', headers = headerss)
+page = requests.get('https://merojob.com/category/it-telecommunication/')
+soup = BeautifulSoup(page.content,"lxml")
+title = soup.find_all('div', class_='card-body')
+titlelist =[]
+for each in title:
+    for each1 in each.find_all('h1'):
+        for each2 in each1.find_all('a'):
+            titlelist.append(each2.text)
 
 
-# name = []
-soup = BeautifulSoup(page.content,"html.parser")
-title = soup.find_all('div', class_='title-wrapper--IaQ0m')
+cleaned_title = [job.strip() for job in titlelist]
+print(cleaned_title)
 
 
-# print(name)
-
-for titles in title:
-    print(titles)
 
 
+
+
+#%%
 
 #%%
