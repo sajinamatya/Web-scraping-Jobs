23c2b96b63d9dcfbc908071a10c7038914d7a8ac
sajin amatya <sajinamatya88@gmail.com>
2024-02-27 21:26:58 +0545

    company name added

--- Web scraping/main.py
+++ Web scraping/main.py
@@ -10,6 +10,7 @@ from random import randint
 
 
 titlelist = []
+company=[]
 
 for page in range(1, 6):
     headerss = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36', 'Accept-langauge': 'en-US, en;q=0.5'}
@@ -21,10 +22,15 @@ for page in range(1, 6):
             for each2 in each1.find_all('a'):
                 titlelist.append(each2.text)
 
+        for view in each.find_all('h3'):
+            for view2 in view.find_all('a'):
+                company.append(view2.text)
+
      sleep(randint(2,10))
 cleaned_title = [job.strip() for job in titlelist]
-print(cleaned_title)
-
+cleaned_company =[companys.strip() for companys in company]
+# print(cleaned_title)
+print(cleaned_company)
 
 
 
