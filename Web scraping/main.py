f4ac81fffdc73c6cde092bd8025deeb761ac2c27
sajin amatya <sajinamatya88@gmail.com>
2024-02-27 09:10:42 +0545

    updated

--- Web scraping/main.py
+++ Web scraping/main.py
@@ -3,18 +3,25 @@
 import requests
 
 from bs4 import BeautifulSoup
-# from time import sleep
-# from random import randint
+from time import sleep
+from random import randint
 
-headerss = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36', 'Accept-langauge': 'en-US, en;q=0.5'}
-page = requests.get('https://merojob.com/category/it-telecommunication/')
-soup = BeautifulSoup(page.content,"lxml")
-title = soup.find_all('div', class_='card-body')
+
+
+
 titlelist = []
-for each in title:
-    for each1 in each.find_all('h1'):
-        for each2 in each1.find_all('a'):
-            titlelist.append(each2.text)
+
+for page in range(1, 6):
+    headerss = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36', 'Accept-langauge': 'en-US, en;q=0.5'}
+    request = requests.get('https://merojob.com/category/it-telecommunication/?page='+str(page))
+    soup = BeautifulSoup(request.content,"lxml")
+    title = soup.find_all('div', class_='card-body')
+    for each in title:
+        for each1 in each.find_all('h1'):
+            for each2 in each1.find_all('a'):
+                titlelist.append(each2.text)
+
+     sleep(randint(2,10))
 cleaned_title = [job.strip() for job in titlelist]
 print(cleaned_title)
 
